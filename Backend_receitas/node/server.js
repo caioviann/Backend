6ae47a16e1413cc34980d1024f26a3722ef793e6
caio henrique <caiohenriqueviana01@gmail.com>
2025-05-07 12:46:13 -0300

    adicionando backend receitas

--- Backend_receitas/node/server.js
+++ Backend_receitas/node/server.js
@@ -1,6 +1,35 @@
 const express = require('express');
-const sequelize = require('./database/db');
-const  = require('./models');
+const sequelize = require('./config/db.js');
+const Ingrediets = require('./models/Ingredient.js');
+
 
 const app = express();
 app.use(express.json());
+
+sequelize.sync().catch(() => {
+    console.log("banco sincronizado");
+});
+app.listen(3000, () => {
+    console.log('Servidor Ouvindo na porta 3000');
+});
+
+
+
+
+
+//app.post('/ingredientes', async (req, res) => {
+//    const { name } = req.body;
+//    const ingredient = await Ingrediets.create({ name });
+//    res.json({ id: ingredient.id , name: ingredient.name});
+//});
+
+//app.get('/ingredientes/:id', async (req, res) => {
+//    const id = req.params.id;
+//    res.send(`Ingrediente Requisitado: ${id}`);
+//});
+
+//app.get('/ingredientes/nome/:name', async (req, res) => {
+//    const ingredient = await Ingrediets.findOne({ where: { name: req.params.name } });
+//    if (ingredient) res.json(ingredient);
+//    else res.status(404).send('Ingrediente não encontrado');
+//  });
