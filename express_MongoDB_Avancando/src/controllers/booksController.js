827e2be71dfecf0f02cb6cfa2834e1a2952ecd4e
Caio Henrique <caiohenriqueviana01@gmail.com>
2025-11-04 11:49:10 -0300

    adicionando alterações

--- express_MongoDB_Avancando/src/controllers/booksController.js
+++ express_MongoDB_Avancando/src/controllers/booksController.js
@@ -1,13 +1,27 @@
 import NotFound from "../errors/NotFound.js";
-import { books } from "../models/index.js";
+import { authors, books } from "../models/index.js";
 
 class BookController {
-
   static bookList = async (req, res, next) => {
     try {
-      const resultBook = await books.find();
+      let { limit = 5, page = 1 } = req.query;
 
-      res.status(200).json(resultBook);
+      limit = parseInt(limit);
+      page = parseInt(page);
+      if(limit > 0 && page > 0) {
+        const resultBook = await books
+          .find()
+          .skip((page - 1) * limit)
+          .limit(limit)
+          .populate("autor")
+          .exec();
+
+        res.status(200).json(resultBook);
+      }else{
+        next(new IncorretRequest);
+      }
+
+      
     } catch (err) {
       next(err);
     }
@@ -18,19 +32,17 @@ class BookController {
       const id = req.params.id;
 
       const resultBook = await books.findById(id);
-      if(resultBook !== null){
+      if (resultBook !== null) {
         res.status(200).send(resultBook);
       } else {
         next(new NotFound("- Id do livro não encontrado"));
       }
-
-     
     } catch (err) {
       next(err);
     }
   };
 
-  static booksRegister = async (req, res, next ) => {
+  static booksRegister = async (req, res, next) => {
     try {
       let livro = new books(req.body);
 
@@ -46,13 +58,12 @@ class BookController {
     try {
       const id = req.params.id;
 
-      const result = await books.findByIdAndUpdate(id, {$set: req.body});
-      if(result !== null){
-        res.status(200).send({message: "Livro atualizado com sucesso"});
+      const result = await books.findByIdAndUpdate(id, { $set: req.body });
+      if (result !== null) {
+        res.status(200).send({ message: "Livro atualizado com sucesso" });
       } else {
         next(new NotFound("- Id do livro não encontrado"));
       }
-
     } catch (err) {
       next(err);
     }
@@ -63,12 +74,11 @@ class BookController {
       const id = req.params.id;
 
       const result = await books.findByIdAndDelete(id);
-      if(result !== null){
-        res.status(200).send({message: "Livro removido com sucesso"});
-      } else{
+      if (result !== null) {
+        res.status(200).send({ message: "Livro removido com sucesso" });
+      } else {
         next(new NotFound("- Id do livro não encontrado"));
       }
-
     } catch (err) {
       next(err);
     }
@@ -76,22 +86,45 @@ class BookController {
 
   static bookListByFilter = async (req, res, next) => {
     try {
-      const { publisher, title } = req.query;
+      const search = await processSearch(req.query);
 
-      const search = {};
-      if (publisher) search.publisher = publisher;
-      if(title) search.title = title;
+      if (search !== null) {
+        const resultBook = await books.find(search).populate("autor");
 
-      const resultBook = await books.find(search);
-
-      res.status(200).send(resultBook);
+        res.status(200).send(resultBook);
+      } else {
+        res.status(200).send("num tem esse autor :(");
+      }
     } catch (err) {
       next(err);
     }
   };
+}
 
+async function processSearch(param) {
+  const { publisher, title, minPages, maxPages, nameAuthor } = param;
 
+  let search = {};
+  if (publisher) search.publisher = publisher;
+  if (title) search.title = { $regex: title, $options: "i" };
 
+  if (minPages || maxPages) search.numberPages = {};
+  // gte = Greater Than or Equal = Maior ou igual que
+  if (minPages) search.numberPages.$gte = minPages;
+  // lte = Less Than or Equal = Menor ou igual que
+  if (maxPages) search.numberPages.$lte = maxPages;
+
+  if (nameAuthor) {
+    const author = await authors.findOne({ name: nameAuthor });
+
+    if (author !== null) {
+      search.autor = author._id;
+    } else {
+      search = null;
+    }
+  }
+
+  return search;
 }
 
 export default BookController;
